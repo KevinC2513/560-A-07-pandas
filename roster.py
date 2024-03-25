a39799da6f208ba2ea092acf2770f0e771a6d78a
KevinC2513 <108152924+KevinC2513@users.noreply.github.com>
2024-03-25 02:58:36 -0400

    Combined roster into player

--- roster.py
+++ roster.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 
-roster = ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "Wojcik", "Washington", "Leo", "Landry"]
-player = {"Last Name": roster,
+player = {"Last Name": ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "Wojcik", "Washington", "Leo", "Landry"],
           "First Name": ["Zayden", "Elliot", "Cormac", "RJ", "Armando", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
           "height": [81, 73, 77, 72, 83, 75, 77, 82, 73, 76],
           "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190],
