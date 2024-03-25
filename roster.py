0ea37d77501e46cdf6296d9d219f06cbb1da875b
KevinC2513 <108152924+KevinC2513@users.noreply.github.com>
2024-03-25 02:32:00 -0400

    0 header

--- roster.py
+++ roster.py
@@ -1,10 +1,5 @@
-
-# https://goheels.com/sports/mens-basketball/roster
-
+import pandas as pd 
 
 roster = ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "Wojcik", "Washington", "Leo", "Landry"]
-print(roster)
-
-for name in roster:
-    print(name)
-    
+data = pd.DataFrame(roster)
+print(data)
