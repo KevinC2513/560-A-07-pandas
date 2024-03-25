85929a624b4a9df3b91b59858e537962c8c31ebb
KevinC2513 <108152924+KevinC2513@users.noreply.github.com>
2024-03-25 03:05:24 -0400

    BMI CSV, BMI creation, BMI rounding

--- roster.py
+++ roster.py
@@ -5,6 +5,13 @@ player = {"Last Name": ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "
           "height": [81, 73, 77, 72, 83, 75, 77, 82, 73, 76],
           "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190],
           }
+
 data = pd.DataFrame(player)
+
+#added the round() function and , 2 to make it round to 2nd deci place.
+data['bmi'] = round((data["weight"]/2.205)/((data["height"]/39.37)**2), 2)
+
 print(data)
+
+data.to_csv("bmi.csv")
 
