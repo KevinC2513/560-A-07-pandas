1ef60853ede0db870d2f938b5274d782f083e70b
KevinC2513 <108152924+KevinC2513@users.noreply.github.com>
2024-03-25 02:56:56 -0400

    Height Weight

--- roster.py
+++ roster.py
@@ -1,6 +1,11 @@
 import pandas as pd 
 
 roster = ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "Wojcik", "Washington", "Leo", "Landry"]
-player = {"Last Name": roster}
+player = {"Last Name": roster,
+          "First Name": ["Zayden", "Elliot", "Cormac", "RJ", "Armando", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
+          "height": [81, 73, 77, 72, 83, 75, 77, 82, 73, 76],
+          "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190],
+          }
 data = pd.DataFrame(player)
 print(data)
+
